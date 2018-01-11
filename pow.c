7cfeb5d9b9817cee7605712af93237913fcef73f
Dharanimanoharan <30828412+Dharanimanoharan@users.noreply.github.com>
2018-01-11 16:47:56 +0530

    Create pow.c

---
+++ pow.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio>
+void main()
+{
+int i,j,k,power=1;
+printf("enter the value of i");
+scanf("%d",&i);
+printf("enter the value of j");
+scanf("%d",&j);
+if(k=0;k<j;k++)
+{
+ power=power*i;
+ printf("\n power of digits");
+ }getch();
+ }
