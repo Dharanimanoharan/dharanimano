38580ce714b23f143ef53777c77d74117ef2324d
Dharanimanoharan <30828412+Dharanimanoharan@users.noreply.github.com>
2018-01-11 16:12:46 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+ char s[5];
+ printf("enter the vowels");
+ scanf("%c",&s);
+ if(s=='A'||s=='a'||s=='E'||s=='e'||s=='I'||s='i'||s=='O'||s=='o'||s=='U'||s=='u')
+ {
+ printf("vowels");
+ }
+else
+{
+ printf("constant");
+ }
