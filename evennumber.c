052dc8c7a0a55c8af367b89a5b72f2cbb17987d2
Dharanimanoharan <30828412+Dharanimanoharan@users.noreply.github.com>
2018-01-18 16:38:23 +0530

    Create evennumber.c

---
+++ evennumber.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int s,t,r;
+ printf("enter the number");
+ scanf("%d",&s,&t);
+ for(r=s;r<t;r++)
+ {
+ if(m%2=0)
+ {
+ printf("even number is");
+ }
+ }
+ getch();
+ }
