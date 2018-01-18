6f7bc6ccdab741bba27bc4f745646ec32a34a4d3
Dharanimanoharan <30828412+Dharanimanoharan@users.noreply.github.com>
2018-01-18 16:30:52 +0530

    Create oddnumber.c

---
+++ oddnumber.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,j,m;
+printf("enter the number");
+scanf("%d",&i,&j);
+for(m=i;m<j;i++)
+{
+if(i%2!=0)
+{
+printf("odd num is");
+}
+}
+getch();
+}
