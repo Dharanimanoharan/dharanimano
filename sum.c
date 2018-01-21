4b0be3e807fa77f8a587aa23f436f73099ede3b1
Dharanimanoharan <30828412+Dharanimanoharan@users.noreply.github.com>
2018-01-21 10:07:32 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+ int i,j,k,m,sum=0;
+ printf(" enter the value ");
+ scanf("%d",&k,m);
+ for(i=1;i<=k;i++)
+ {
+  printf("input is",i);
+  }
+  for(j=1;j<=m;j++)
+  {
+  sum=sum+m;
+    }
+  printf("The sum is%d",sum);
+  }
