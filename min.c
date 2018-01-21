2fb4b1c21e74ffff1eb59f992893914e753fb6ac
Dharanimanoharan <30828412+Dharanimanoharan@users.noreply.github.com>
2018-01-21 12:00:02 +0530

    Create min.c

---
+++ min.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int n,a[i],min,i;
+printf("\n enter the number");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+min=a[0];
+for(i=0;i<n;i++)
+{
+ if(min>a[i])
+ {
+  min=a[i];
+  }
+  printf("\n min num is %d",max);
+  }
+  }
