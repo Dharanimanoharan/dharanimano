934e3efe8407433596b81673acc3d1d4a82cfbc1
Dharanimanoharan <30828412+Dharanimanoharan@users.noreply.github.com>
2018-01-21 11:54:04 +0530

    Create max.c

---
+++ max.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int n,a[i],max,i;
+printf("\n enter the number");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+max=a[0];
+for(i=0;i<n;i++)
+{
+ if(max<a[i])
+ {
+  max=a[i];
+  }
+  printf("\n max num is %d",max);
+  }
+  }
