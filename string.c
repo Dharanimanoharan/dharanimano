e98b82250e33b5a2cb1cf5dafe3c1c1425c830c8
Dharanimanoharan <30828412+Dharanimanoharan@users.noreply.github.com>
2018-01-23 16:45:22 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ char s[50];
+ int i,n;
+ printf("\n enter the string");
+ scanf("%s",&s);
+ printf("\n Enter the number");
+ scanf("%d",&n);
+ for(i=0;i<n;i++)
+ {
+  printf("\n The string is %s",s);
+  }
+  getch();
+  }
