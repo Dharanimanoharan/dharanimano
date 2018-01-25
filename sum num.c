7643ad7aeb1f88ae918d491866cd24f007d68611
Dharanimanoharan <30828412+Dharanimanoharan@users.noreply.github.com>
2018-01-25 16:56:53 +0530

    Update sum num.c

--- sum num.c
+++ sum num.c
@@ -3,8 +3,10 @@
 void main()
 {
  int a,s,k;
- printf("enter the two num"); 
- scanf("%d%d",&a,&s);
+ printf("enter the num"); 
+ scanf("%d",&a);
+ printf("\n Enter the num");
+ scans("%d",&s);
  k=a+s;
  printf("\n The sum is %d",k);
  }
