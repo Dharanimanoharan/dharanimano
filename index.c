536901a4546455e4e90bf53f46e397664f8cfe46
Dharanimanoharan <30828412+Dharanimanoharan@users.noreply.github.com>
2018-02-01 09:01:53 +0000

    Create index.c

---
+++ index.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int m,n,a[100];
+clrscr();
+printf("\n enter the number");
+scanf("%d",&n);
+for(m=0;m<n;m++)
+{
+scanf("%d",&a[i]);
+}
+for(m=0;m<n;m++)
+{
+printf("\n%d%d",a[i],m);
+}
+getch();
+}
