706820502808aadeb38c450a8c11e930b840d1e5
Dharanimanoharan <30828412+Dharanimanoharan@users.noreply.github.com>
2018-02-08 16:36:42 +0530

    Create word.c

---
+++ word.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+ int i,count;
+ char b[50];
+ printf("\n Enter the string");
+ scanf("%[^\n]s",&b);
+ for(i=0;b[i]!='\0';i++)
+ {
+  if(b[i]==' ')
+  {
+  count=count+1;
+  }
+  }
+  printf("\n The number of word is %d",count);
+  }
