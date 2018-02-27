fa7e930afaca235e2c320e683e77f29608b94d61
Dharanimanoharan <30828412+Dharanimanoharan@users.noreply.github.com>
2018-02-27 16:39:47 +0530

    Create a.c

---
+++ a.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c,d;
+    printf("\n Enter the value of a");
+    scanf("%d",&a);
+    printf("\n Enter the value of b");
+    scanf("%d",&b);
+    printf("\n Enter the value of c");
+    scanf("%d",&c);
+    d=(a*b)/c;
+    printf("\n %d",d);
+}
