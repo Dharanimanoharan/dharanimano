f2443718e714877ad78e7df704731904fc0c0ef3
Dharanimanoharan <30828412+Dharanimanoharan@users.noreply.github.com>
2018-03-02 15:08:59 +0530

    Create joint.c

---
+++ joint.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    printf("\n Enter the value of a");
+    scanf("%d",&a);
+    printf("\n Enter the value of b");
+    scanf("%d",&b);
+    printf("\n %d%d",a,b);
+}
