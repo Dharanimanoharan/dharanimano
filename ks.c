f5a1ad8417c37bde6ca1b86a3e14a3a1c7932a35
Dharanimanoharan <30828412+Dharanimanoharan@users.noreply.github.com>
2018-03-02 15:03:50 +0530

    Create ks.c

---
+++ ks.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    int a[20],i,b,k,count=0;
+    printf("\n Enter the value of b");
+    scanf("%d",&b);
+    printf("\n Enter the value of k");
+    scanf("%d",&k);
+    for(i=1;i<b;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=1;i<b;i++)
+    {
+        if(a[k]==a[i])
+        {
+        printf("\n %d",a[i]);
+        }
+    
+    }
+}
