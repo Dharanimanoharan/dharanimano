59eeea611b7d15ca30dbadf0698678c128361d2c
Dharanimanoharan <30828412+Dharanimanoharan@users.noreply.github.com>
2018-03-02 14:48:02 +0530

    Create ck.c

---
+++ ck.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    int a[20],i,b,k,count=0;
+    printf("\n Enter the value of b");
+    scanf("%d",&b);
+    printf("\n Enter the value of k");
+    scanf("%d",&k);
+    for(i=0;i<b;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<b;i++)
+    {
+        if(a[i]==k)
+        {
+            printf("\n YES");
+        }
+    
+    }
+}
