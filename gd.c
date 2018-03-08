1f1177082805bf54b44510c34a5a88f5475110bc
Dharanimanoharan <30828412+Dharanimanoharan@users.noreply.github.com>
2018-03-08 16:39:06 +0530

    Create gd.c

---
+++ gd.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    
+    char a[10],b[10];
+    printf("\n Enter the word");
+    scanf("%s",&a);
+    printf("\n Enter the word");
+    scanf("%s",&b);
+    printf("%s%s",a,b);
+}
