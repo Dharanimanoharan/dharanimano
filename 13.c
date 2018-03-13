c85bc5b2c1ee6ce870c0256f6deb562a022305f9
Dharanimanoharan <30828412+Dharanimanoharan@users.noreply.github.com>
2018-03-13 16:33:36 +0530

    Create 13.c

---
+++ 13.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("\n Enter the value of n");
+    scanf("%d",&n);
+    if(n%13==0)
+    {
+        printf("\n Yes");
+    }
+    else
+    {
+        printf("\n No");
+    }
+}
