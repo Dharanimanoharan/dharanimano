e2edfa33a0f709ba5c681790b5eefff905d39125
Dharanimanoharan <30828412+Dharanimanoharan@users.noreply.github.com>
2018-03-15 08:32:45 +0000

    Create lr.c

---
+++ lr.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+    int n,L,R,i,count;
+    printf("\n Enter the value of n");
+    scanf("%d",&n);
+    printf("\n Enter the value of L");
+    scanf("%d",&L);
+    printf("\n Enter the value of R");
+    scanf("%d",&R);
+for(i=l;i<r;i++)
+{
+    if(n==i)
+    {
+        printf("\n Yes");
+       count++;
+    }
+}
+if(count==0)
+{
+    printf("\n No");
+    
+}
+}
+
