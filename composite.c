ab336aeae5e4a83976f89bad4d06b57549f511a9
Dharanimanoharan <30828412+Dharanimanoharan@users.noreply.github.com>
2018-03-15 08:49:27 +0000

    Create composite.c

---
+++ composite.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main()
+{
+int j,count=0,n;
+printf("\n enter the number);
+scanf("%d",&n);
+for(j=2;j<n;j++)
+{
+if(n%j==0)
+{
+count++;    
+}
+    }
+if(count!=0)
+{
+    printf("yes");
+}
+else
+{
+    printf("no");
+}
+}
+
