4ce78a953f1723eb5352b6962576903d5d6a95dd
Dharanimanoharan <30828412+Dharanimanoharan@users.noreply.github.com>
2018-03-20 16:11:15 +0530

    Create mod.c

---
+++ mod.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c,s;
+    printf("\n Enter the number a");
+    scanf("%d",&a);
+    printf("\n Enter the number b");
+    scanf("%d",&b);
+    printf("\n Enter the number c");
+    scanf("%d",&c);
+    s=(a*b)%c;
+    printf("\n %d",s);
+    
+    
+}
